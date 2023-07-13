75f83cef4693d02791b33067b9ac1301682470bc
= <tristanmayofreire@gmail.com>
2023-07-13 09:52:31 +0200

    Scrape as many as you like!

--- inject.js
+++ inject.js
@@ -1,49 +1,70 @@
 const qs = [];
-const qsExtractLimit = 5;
 
-const container = document.querySelector('div[jsname="N760b"]');
+RunScript(10);
 
-// LETS GET ALL THE ONES THAT SHOW AT START
-let paas;
+async function RunScript(limit) {
+  const container = document.querySelector('div[jsname="N760b"]');
 
-paas = Array.from(container.querySelectorAll('div[jsname="yEVEwb"]'));
-for (let i = 0; i < paas.length; i++) {
-  if (!paas[i]) {
-    // find last element
-    // click it
-    // wait a bit
+  // LETS GET ALL THE ONES THAT SHOW AT START
+  let paas;
+
+  paas = getPAAS();
+
+  function getPAAS() {
+    return Array.from(container.querySelectorAll('div[jsname="yEVEwb"]'));
   }
-  const qEl = paas[i].querySelector(".CSkcDe");
-  const aEl = paas[i].querySelector(".wDYxhc");
-  let url, urlEl;
-  try {
-    const urlChildEl = paas[i].querySelector(".LC20lb");
-    if (urlChildEl) {
-      urlEl = urlChildEl.parentNode;
-    } else {
-      const urlParent = paas[i].querySelector(".Xv4xee");
-      if (urlParent) {
-        urlEl = urlParent.querySelector("a");
+  for (let i = 0; i < limit; i++) {
+    if (!paas[i]) {
+      try {
+        // find last element
+        const lastEl = paas[i - 1];
+        // get btn & click it
+        const btn = lastEl.querySelector(".dnXCYb");
+        btn.click();
+        // wait a bit
+        await randomSleep();
+        paas = getPAAS();
+        if (!paas[i]) {
+          console.log("not found new elements");
+          break;
+        }
+      } catch (e) {
+        console.log(e);
+        break;
       }
     }
-    url = urlEl.href;
-  } catch (e) {
-    console.log(e);
+    const qEl = paas[i].querySelector(".CSkcDe");
+    const aEl = paas[i].querySelector(".wDYxhc");
+    let url, urlEl;
+    try {
+      const urlChildEl = paas[i].querySelector(".LC20lb");
+      if (urlChildEl) {
+        urlEl = urlChildEl.parentNode;
+      } else {
+        const urlParent = paas[i].querySelector(".Xv4xee");
+        if (urlParent) {
+          urlEl = urlParent.querySelector("a");
+        }
+      }
+      url = urlEl.href;
+    } catch (e) {
+      console.log(e);
+    }
+
+    const q = getText(qEl);
+    const a = getText(aEl);
+    const aHtml = aEl.innerHTML;
+
+    qs.push({
+      q,
+      a,
+      aHtml,
+      url,
+    });
   }
 
-  const q = getText(qEl);
-  const a = getText(aEl);
-  const aHtml = aEl.innerHTML;
-
-  qs.push({
-    q,
-    a,
-    aHtml,
-    url,
-  });
+  console.log(qs);
 }
-
-console.log(qs);
 
 function getText(el) {
   return el.innerText || el.textContent;
@@ -57,3 +78,15 @@ function getText(el) {
 // a -> .wDYxhc
 // child url -> .LC20lb | LC20lb MBeuO DKV0Md
 // el.scrollIntoView(true)
+async function randomSleep(min = 600, max = 1300) {
+  return await sleep(randomIntFromInterval(min, max));
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
